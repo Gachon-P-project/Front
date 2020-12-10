c3c270c290ee9a36405bcbe077c0ec6775efe843
Jun <wnstn2913@naver.com>
2020-12-11 08:38:28 +0900

    3's commit

---
+++ app/src/main/java/com/hfad/gamo/ClickedBoard/ClickedPostingActivity.java
@@ -0,0 +1,16 @@
+package com.hfad.gamo.ClickedBoard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hfad.gamo.R;
+
+public class ClickedPostingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicked_posting);
+    }
+}
---
+++ app/src/main/java/com/hfad/gamo/ClickedBoard/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.hfad.gamo.ClickedBoard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hfad.gamo.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+    }
+}
