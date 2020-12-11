9466bb0b7d26b2ddbfd92db9fb6f9f4dbf3d36d0
jinyoung1129 <jy11290@naver.com>
2020-12-11 21:39:16 +0900

    Add Function
    Writing title Wacther 추가

--- app/src/main/java/com/hfad/gamo/ClickedBoard/WritingActivity.java
+++ app/src/main/java/com/hfad/gamo/ClickedBoard/WritingActivity.java
@@ -1,13 +1,19 @@
 package com.hfad.gamo.ClickedBoard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.hfad.gamo.R;
 
 public class WritingActivity extends AppCompatActivity {
@@ -22,7 +28,30 @@ public class WritingActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("글쓰기");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back);
+
+        final AppCompatEditText title_edit = (AppCompatEditText) findViewById(R.id.board_write_title_edit);
+
+        title_edit.addTextChangedListener(titleWatcher);
     }
+
+    TextWatcher titleWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable ed) {
+            if(ed.length() > 60){
+                Toast.makeText(WritingActivity.this, "제목은 60자를 넘을 수 없습니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
