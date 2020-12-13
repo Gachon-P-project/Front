aeb8b2604e9f4c37c476a34d792b7d8762c7ebc0
jinyoung1129 <jy11290@naver.com>
2020-12-13 21:59:09 +0900

    Add Layout
    글 검색 레이아웃 추가

--- app/src/main/java/com/hfad/gamo/ClickedBoard/SearchActivity.java
+++ app/src/main/java/com/hfad/gamo/ClickedBoard/SearchActivity.java
@@ -3,6 +3,8 @@ package com.hfad.gamo.ClickedBoard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.hfad.gamo.R;
 
@@ -13,5 +15,12 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        ImageView back_btn = (ImageView) findViewById(R.id.btn_back);
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
