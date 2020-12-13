8b696712041640a50131af5b7bed918350550c47
jinyoung1129 <jy11290@naver.com>
2020-12-14 00:16:45 +0900

    Add Function
    글 검색 GET 테스트 완료

--- app/src/main/java/com/hfad/gamo/ClickedBoard/SearchActivity.java
+++ app/src/main/java/com/hfad/gamo/ClickedBoard/SearchActivity.java
@@ -3,12 +3,28 @@ package com.hfad.gamo.ClickedBoard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.hfad.gamo.R;
+import com.hfad.gamo.VolleyForHttpMethod;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SearchActivity extends AppCompatActivity {
+    private static JSONObject requestJSONObject = new JSONObject();
+    private static VolleyForHttpMethod volley;
+    private static JSONArray requestJSONArray = new JSONArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +38,40 @@ public class SearchActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        final EditText search_edit = (EditText) findViewById(R.id.edit);
+        volley = new VolleyForHttpMethod(Volley.newRequestQueue(getApplicationContext()));
+        search_edit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                switch (i) {
+                    case EditorInfo.IME_ACTION_SEARCH:
+//                        Toast.makeText(getApplicationContext(), "검색"+search_edit.getText().toString(), Toast.LENGTH_LONG).show();
+
+                        String url = "http://192.168.254.2:17394/board/select/컴퓨터구조/search/"+search_edit.getText().toString();
+
+                        volley.getJSONArray(url, new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                for (int i = 0; i < response.length(); i++) {
+                                    try {
+                                        requestJSONObject = response.getJSONObject(i);
+                                        requestJSONArray.put(requestJSONObject);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                Log.d("GET RESPONSE", ""+requestJSONArray);
+                            }
+                        });
+
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+
     }
 }
