d4b3a1e309b503f0319d83d8b336f7dd7e1eb243
jinyoung1129 <jy11290@naver.com>
2020-12-19 18:23:59 +0900

    TEST GIT
    TEST

--- app/src/main/java/com/hfad/gamo/MainActivity.java
+++ app/src/main/java/com/hfad/gamo/MainActivity.java
@@ -20,7 +20,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import static com.hfad.gamo.DataIOKt.appConstantPreferences;
 
 public class MainActivity extends AppCompatActivity {
-
+    // TEST GIT
     private FragmentManager fm = getSupportFragmentManager();
     private com.hfad.gamo.timeTable.TimeTableFragment f_TimeTable2 = new com.hfad.gamo.timeTable.TimeTableFragment();
     private NotificationFragment f_Notification = new NotificationFragment();
