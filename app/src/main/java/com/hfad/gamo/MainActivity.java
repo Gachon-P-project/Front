044463627a7e9a0674247891ca4d3e60bf473480
jinyoung1129 <jy11290@naver.com>
2020-12-29 00:14:48 +0900

    Add route
    토큰 등록 라우트 추가

--- app/src/main/java/com/hfad/gamo/MainActivity.java
+++ app/src/main/java/com/hfad/gamo/MainActivity.java
@@ -47,8 +47,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        default_url = "http://192.168.50.146:17394";
-        //default_url = "http://112.148.161.36:17394";
+        //default_url = "http://192.168.50.146:17394";
+        default_url = "http://112.148.161.36:17394";
 //        default_url = "http://192.168.254.2:17394";
 
         sharedPreferences = getSharedPreferences(appConstantPreferences, Context.MODE_PRIVATE);
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                         // 토큰이 없었거나, 기존 토큰과 다르다면 SharedPreferences 에 저장.
                         // 서버에 number, token 값 전송
                         if (!(sharedPreferences.getString("token", "null").equals(token))) {
-                            String tokenUrl = "Good";
+                            String tokenUrl = default_url+"/token/add";
                             sharedPreferences.edit().putString("token", token).apply();
 
                             JSONObject jsonObject = new JSONObject();
