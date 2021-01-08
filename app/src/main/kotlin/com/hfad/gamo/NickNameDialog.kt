832cff5b47da7410cfe123fa8bd26e42f81dc215
bansakdo <bansakdo@gmail.com>
2021-01-09 04:39:10 +0900

    Update dialog_nickname.xml
    Add cancel button when update nickname

--- app/src/main/kotlin/com/hfad/gamo/NickNameDialog.kt
+++ app/src/main/kotlin/com/hfad/gamo/NickNameDialog.kt
@@ -22,6 +22,8 @@ class NickNameDialog(baseLoginActivity: Activity) {
     private lateinit var nicknameEditText: EditText
     private lateinit var nicknameExistentTextView: TextView
     private lateinit var nicknameActionButton: Button
+    private lateinit var btnNicknameUpdate: Button
+    private lateinit var btnCancel: Button
     private var volley: VolleyForHttpMethod? = null
     private val nicknameExistent = 15
     private lateinit var nickname: String
@@ -42,6 +44,10 @@ class NickNameDialog(baseLoginActivity: Activity) {
 //        nicknameRegisterButton = dlg.findViewById(R.id.nickname_register)
 //
 //        nicknameExistentTextView.visibility = View.INVISIBLE
+
+        btnNicknameUpdate.visibility = View.GONE
+        btnCancel.visibility = View.GONE
+        nicknameActionButton.visibility = View.VISIBLE
 
         nicknameActionButton.setOnClickListener {
             nicknameActionButton.isClickable = false
@@ -104,7 +110,10 @@ class NickNameDialog(baseLoginActivity: Activity) {
         nicknameEditText = dlg.findViewById(R.id.nickname_edit_text)
         nicknameExistentTextView = dlg.findViewById(R.id.nickname_existent)
         nicknameActionButton = dlg.findViewById(R.id.nickname_register)
+        btnNicknameUpdate = dlg.findViewById(R.id.button_dialog_nickname_update)
+        btnCancel = dlg.findViewById(R.id.button_dialog_nickname_cancel)
         nicknameExistentTextView.visibility = View.INVISIBLE
+
 
 //        다이얼로그 창 크기 조절
         val params: ViewGroup.LayoutParams? = dlg?.window?.attributes
@@ -127,10 +136,13 @@ class NickNameDialog(baseLoginActivity: Activity) {
         var previousNickname = getSharedItem<String>("nickname")
         nicknameEditText.setText(previousNickname)
         nicknameEditText.hint = previousNickname
-        nicknameActionButton.text = "닉네임 변경"
+//        nicknameActionButton.text = "닉네임 변경"
+        btnNicknameUpdate.visibility = View.VISIBLE
+        btnCancel.visibility = View.VISIBLE
+        nicknameActionButton.visibility = View.GONE
         dlg.setCancelable(true)
 
-        nicknameActionButton.setOnClickListener {
+        btnNicknameUpdate.setOnClickListener {
             nicknameActionButton.isClickable = false
             nickname = nicknameEditText.text.toString()
             nickname = nickname.replace(" ", "")
@@ -163,6 +175,9 @@ class NickNameDialog(baseLoginActivity: Activity) {
                     }
                 }
             }
+        }
+        btnCancel.setOnClickListener {
+            dlg.dismiss()
         }
         dlg.show()
     }
