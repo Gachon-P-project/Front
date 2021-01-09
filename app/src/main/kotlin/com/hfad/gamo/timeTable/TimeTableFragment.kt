8be01e7d6fff0d4ab33097a81652e1a1636f6a3b
bansakdo <bansakdo@gmail.com>
2021-01-10 00:51:59 +0900

    Fix getting TimeTable data 2

--- app/src/main/kotlin/com/hfad/gamo/timeTable/TimeTableFragment.kt
+++ app/src/main/kotlin/com/hfad/gamo/timeTable/TimeTableFragment.kt
@@ -59,6 +59,7 @@ class TimeTableFragment : TimeTableContract.View() {
     private val day = nowDate.format(DateTimeFormatter.ofPattern("dd"))
     private lateinit var lastDaySaved: String
     private lateinit var userNo: String
+    private lateinit var yearOfAdmission: String
     private var semester = when (month.toInt()) {
         3, 4, 5, 6 -> "10"      // 1학기
         9, 10, 11, 12 -> "20"   // 2학기
@@ -67,7 +68,6 @@ class TimeTableFragment : TimeTableContract.View() {
         else -> "00"            // 에러
     }
 
-    private val yearOfAdmission = userNo.substring(0, 4);
 
 
 
@@ -77,6 +77,7 @@ class TimeTableFragment : TimeTableContract.View() {
         sharedPreferences = this.context?.getSharedPreferences(appConstantPreferences, Context.MODE_PRIVATE)!!
         lastDaySaved = getSharedItem("lastDaySavingTimeTable", "")
         userNo = getSharedItem<String>("number")
+        yearOfAdmission= userNo.substring(0, 4);
     }
 
     override fun onCreateView(
