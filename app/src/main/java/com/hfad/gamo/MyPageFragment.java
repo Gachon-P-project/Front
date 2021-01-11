378ae31d796193bea5b88ea6a1061a94972e705e
bansakdo <bansakdo@gmail.com>
2021-01-11 16:10:31 +0900

    Update dialogs layout

--- app/src/main/java/com/hfad/gamo/MyPageFragment.java
+++ app/src/main/java/com/hfad/gamo/MyPageFragment.java
@@ -138,6 +138,8 @@ public class MyPageFragment extends Fragment {
                 MyPageFragment.InfoDialog dialog = new MyPageFragment.InfoDialog(getActivity());
                 dialog.setTvInfo(getString(R.string.version));
                 dialog.setTvTitle("앱 버전");
+                dialog.setDialogHeight(0.3);
+                dialog.setTvInfoAlignment(View.TEXT_ALIGNMENT_CENTER);
                 dialog.show();
 
             }
@@ -149,6 +151,8 @@ public class MyPageFragment extends Fragment {
                 MyPageFragment.InfoDialog dialog = new MyPageFragment.InfoDialog(getActivity());
                 dialog.setTvInfo(getString(R.string.inquiry));
                 dialog.setTvTitle("문의하기");
+                dialog.setDialogHeight(0.3);
+                dialog.setTvInfoAlignment(View.TEXT_ALIGNMENT_CENTER);
                 dialog.show();
             }
         });
@@ -178,7 +182,7 @@ public class MyPageFragment extends Fragment {
             public void onClick(View v) {
                 MyPageFragment.InfoDialog dialog = new MyPageFragment.InfoDialog(getActivity());
                 dialog.setTvInfo(getString(R.string.opensource_license));
-                dialog.setTvTitle("오픈소스 라이선스");
+                dialog.setTvTitle("오픈소스 라이센스");
                 dialog.show();
             }
         });
@@ -264,7 +268,6 @@ public class MyPageFragment extends Fragment {
         public void create() {
             super.create();
         }
-
         public void setTitle(String s) {
             tvTitle.setText(s);
         }
@@ -294,6 +297,7 @@ public class MyPageFragment extends Fragment {
         Context context;
 //        TextView btnDialogPositive;
         Button btnDialogPositive;
+        private WindowManager.LayoutParams params = this.getWindow().getAttributes();
 
         public InfoDialog(@NonNull Context context) {
             super(context);
@@ -302,7 +306,7 @@ public class MyPageFragment extends Fragment {
 
             Objects.requireNonNull(getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-            tvInfo = findViewById(R.id.tvDialogInfo);
+            tvInfo = findViewById(R.id.tv_dialog_info_content);
             tvTitle = findViewById(R.id.tv_dialog_info_title);
 //            btnDialogPositive = findViewById(R.id.tvButtonDialogPositive);
             btnDialogPositive = findViewById(R.id.buttonDialogPositive);
@@ -314,7 +318,6 @@ public class MyPageFragment extends Fragment {
                 }
             });
 
-            WindowManager.LayoutParams params = this.getWindow().getAttributes();
             params.width = (int) (deviceWidth * 0.95);
             params.height = (int)(deviceHeight * 0.7);
             this.onWindowAttributesChanged(params);
@@ -334,6 +337,14 @@ public class MyPageFragment extends Fragment {
 
         public void setTvTitle(String s) {
             this.tvTitle.setText(s);
+        }
+
+        public void setDialogHeight(double d) {
+            params.height = (int)(deviceHeight * d);
+            this.onWindowAttributesChanged(params);
+        }
+        public void setTvInfoAlignment(int text_alignment) {
+            tvInfo.setTextAlignment(text_alignment);
         }
     }
 
